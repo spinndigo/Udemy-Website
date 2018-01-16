6ce75b75e730608e53a472b6d940b6ffa4e113de
spinosajoseph <spinosajoseph@gmail.com>
2018-01-16 14:29:13 -0500

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,151 @@
+/* show and hide the navigation bar */
+
+$(document).ready(function() {
+      'use strict';
+                  
+       $(window).scroll(function() {
+        
+           'use strict';
+           
+          if($(window).scrollTop() < 80 ) {
+              
+              $('.navbar').css ({
+                  'margin-top': '-100px',
+                  'opacity': '0'
+              });
+           
+          
+            $('.navbar-default').css({
+               
+               'background-color': 'rgba(59,59,59,0)'
+           });
+           
+           
+          } // end if
+           
+         else{
+              $('.navbar').css ({
+                   
+                   'margin-top': '0px',
+                   'opacity': '1'
+               });
+               
+           
+           $('.navbar-default').css({
+               
+               'background-color': 'rgba(59,59,59,0.7)',
+               'border-color':'#444'
+           });
+           
+        
+         
+         $('.navbar-brand img').css({
+             'height': '35px',
+             'padding-top': '0px'
+         });
+         
+             
+        $('.navbar-nav > li > a').css({
+            'padding-top': '15px'
+            
+        });
+         
+         
+         
+         
+         
+         } // else
+           
+       });
+    
+
+
+});
+
+
+
+// SMOOTH SCROLL ====================================================================
+
+$(document).ready(function() {
+        
+    'use strict';
+          
+    
+$('.nav-item, #scroll-to-top').click(function() {
+    if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        
+    if(target.length) {
+        $('html, body').animate({
+           scrollTop: target.offset().top}, 1000);
+        
+        return false;
+        }
+    }
+});
+    
+        
+        
+ });
+    
+    
+// ACTIVE MENU ITEM ON CLICK =========================== 
+
+
+
+$(document).ready(function() {
+    
+    
+    'use strict';
+    
+    $('.navbar-nav li a').click(function(){
+        
+        'use strict';
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
